e9b881cb6ea93a49fd5113f69badb3d683ddd968
BaalajiVS <v.s.baalaji95@gmail.com>
2019-03-14 02:26:35 -0400

    working for 1 message in 1 AVD

--- PA2/GroupMessenger2/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger2/GroupMessengerActivity.java
+++ PA2/GroupMessenger2/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger2/GroupMessengerActivity.java
@@ -16,6 +16,7 @@ import android.widget.TextView;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -117,106 +118,137 @@ public class GroupMessengerActivity extends Activity{
         return true;
     }
     private class ServerTask extends AsyncTask<ServerSocket, String, Void> {
+
         @Override
         protected Void doInBackground(ServerSocket... sockets) {
+
             ServerSocket serverSocket = sockets[0];
-            //Log.d(TAG, "Entered server class");
+
             while (true) {
                 try {
                     Socket socket = serverSocket.accept();
-                    ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-                    ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-                    //Log.e(TAG, "Input stream for the socket is created. Status " + in.ready());
-                    ArrayList<Object> msgRecieved = (ArrayList<Object>) in.readObject();
-                    if (null != msgRecieved) {
+
+                    DataInputStream in = new DataInputStream(socket.getInputStream());
+                    String inp = in.readUTF();
+                    //in.close(); //as closing either input or output stream closes the socket, the last close() statement alone is executed
+
+                    if (null != inp) {
+                        ArrayList<Object> msgRecieved = str_to_arrlist(inp);
+
                         Log.d("SERVER", "Message received : " + msgRecieved.toString());
                         SEQUENCE += 1;
-                        //ArrayList<Object> input_list = new ArrayList<Object>(Arrays.asList(msgRecieved.split(",")));
-                        if (msgRecieved.size() == 3) {   //<msg, MSG_COUNTER, MyPort>
-                            Log.d("SERVER",msgRecieved + "\t 2");
-                            msgRecieved.add(SEQUENCE);
-                            out.writeObject(msgRecieved); //<MSG, MSG_ID, P_ID, SEQUENCE>
-                            Log.d("SERVER",msgRecieved.toString() + "\t 3");
-                            //adding the output message to priority queue
-                            msgRecieved.add("NOT_FINAL");
-                            BUFFER_QUEUE.add(msgRecieved); //<MSG, MSG_ID, P_ID, SEQUENCE, "NOT_FINAL">
-                        }
 
-                        else if (msgRecieved.size() == 4){   //<msg, MSG_COUNTER, MyPort, Final_sequence number>
+                        if (msgRecieved.size() == 3) {   //<msg, MSG_COUNTER, MyPort>
+
+                            Log.d("SERVER", msgRecieved.toString() + "\t 2"); //<msg, MSG_COUNTER, MyPort>
+
+                            msgRecieved.add(SEQUENCE);
+
+                            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                            out.writeUTF(msgRecieved.toString()); //<MSG, MSG_ID, P_ID, SEQUENCE>
+                            out.flush();
+                            //Thread.sleep(200);
+                            out.close();
+                            in.close();
+                            socket.close();
+
+                            Log.d("SERVER", msgRecieved.toString() + "\t 3"); //<MSG, MSG_ID, P_ID, SEQUENCE>
+
+                            //adding the output message to priority queue
+                            msgRecieved.add(false);
+                            BUFFER_QUEUE.add(msgRecieved); //<MSG, MSG_ID, P_ID, SEQUENCE, "NOT_FINAL">
+
+                        } else if (msgRecieved.size() == 4) {   //<msg, MSG_COUNTER, MyPort, Final_sequence number>
+
                             //updating the priority queue
-                            String INPUT_MSG = msgRecieved.get(0).toString().trim();
-                            String INPUT_MSG_ID = msgRecieved.get(1).toString().trim();
-                            String INPUT_PORT = msgRecieved.get(2).toString().trim();
-                            Integer MSG_SEQ = Integer.parseInt(msgRecieved.get(3).toString());
+
+                            Log.d("SERVER", msgRecieved.toString() + "\t 7"); //<msg, MSG_COUNTER, MyPort, Final_sequence number>
+
+                            String INPUT_MSG = (String) msgRecieved.get(0);
+                            Integer INPUT_MSG_ID = (Integer) msgRecieved.get(1);
+                            String INPUT_PORT = (String) msgRecieved.get(2);
+                            Integer MSG_SEQ = (Integer) msgRecieved.get(3);
+
                             int n = BUFFER_QUEUE.size();
                             int i = 0;
-                            while(i < n){
+
+                            while (i < n) {
                                 ArrayList<Object> arr = BUFFER_QUEUE.poll();
-                                //System.out.println(arr.toString());
-                                if (arr.get(0).toString().trim() == INPUT_MSG && arr.get(1).toString().trim() == INPUT_MSG_ID && arr.get(2).toString().trim() == INPUT_PORT){
+                                Log.d("SERVER", arr.toString() + "\t  8.0");
+                                if (arr.get(0).toString().trim().equals(INPUT_MSG) && arr.get(1).toString().trim().equals(INPUT_MSG_ID.toString()) && arr.get(2).toString().trim().equals(INPUT_PORT)) {
                                     arr.set(3, MSG_SEQ);
-                                    arr.set(4,"FINAL");
+                                    arr.set(4, true);
+                                    Log.d("SERVER", arr.toString() + "\t 8");
                                 }
                                 BUFFER_QUEUE.add(arr);
-                                i+=1;
+                                i += 1;
                             }
+
+                            in.close();
+                            socket.close();
+
 
                             //delivering all the FINAL messages in BUFFER
                             ArrayList<Object> arr = BUFFER_QUEUE.peek();
-                            String STATUS = arr.get(4).toString().trim();
-                            while(STATUS == "FINAL"){
+                            boolean STATUS = Boolean.parseBoolean(arr.get(4).toString().trim());
+
+                            while (STATUS == true) {
                                 ArrayList<Object> delivered = BUFFER_QUEUE.poll();
+                                Log.d("SERVER", delivered.toString() + "\t 9");
+
                                 publishProgress(delivered.get(0).toString().trim(), delivered.get(3).toString().trim()); //message to display in UI
                                 Log.i("DELIVERY", delivered.toString());
 
-                                arr = BUFFER_QUEUE.peek();
-                                STATUS = arr.get(4).toString().trim();
-
+                                if (BUFFER_QUEUE.size() != 0) {
+                                    arr = BUFFER_QUEUE.peek();
+                                    STATUS = Boolean.parseBoolean(arr.get(4).toString().trim());
+                                } else { break; }
                             }
-                        SEQUENCE = MSG_SEQ > SEQUENCE? MSG_SEQ : SEQUENCE; //Updating SEQUENCE
+
+                            SEQUENCE = MSG_SEQ > SEQUENCE ? MSG_SEQ : SEQUENCE; //Updating SEQUENCE
+
                         }
-                        //publishProgress(msgRecieved);
-                        //out.writeUTF("ACK");
 
-                        //Log.d("SERVER", "ACK sent for message : " + msgRecieved);
+                    } else {
+                        Log.e("SERVER", "Message received in else part : " + inp);
                     }
-                    else{Log.e("SERVER", "Message received in else part : " + msgRecieved);}
 
-                    in.close();
-                    out.flush();
-                    out.close();
-                    socket.close();
 
+                } catch (EOFException e) {
+                    e.printStackTrace();
+                    Log.e("SERVER", "ServerTask IOException");
+                    //continue;
                 } catch (IOException e) {
-                    System.out.println(e);
-                } catch (ClassNotFoundException e) {
                     e.printStackTrace();
                 }
             }
         }
 
         protected void onProgressUpdate(String... strings) {
+
             Log.e(TAG, "Message in onProgressUpdate : " + strings[0]);
+
             String strReceived = strings[0].trim();
             Integer ID = Integer.parseInt(strings[1].trim());
+
             TextView remoteTextView = (TextView) findViewById(R.id.textView1);
             remoteTextView.append("\t\t\t" + strReceived + "\n");
 
             ContentValues cv = new ContentValues();
-            //cv.put(key,Integer.toString(id));
             cv.put(key,ID.toString());
             cv.put(value,strReceived);
             Uri t_uri = getContentResolver().insert(mUri,cv);
 
             Log.d("URI",t_uri.toString());
             Log.d("insert",cv.toString());
-            //id++;
 
             return;
         }
     }
     private class ClientTask extends AsyncTask<String, Void, Void> {
+
         @Override
+
         protected Void doInBackground(String... msgs) {
             try {
 
@@ -228,56 +260,63 @@ public class GroupMessengerActivity extends Activity{
                 int final_sequence_number = -1;
 
                 if (null != msgToSend || !msgToSend.isEmpty()) {
+
                     ArrayList<Object> msg = new ArrayList<Object>();
                     msg.add(msgToSend);
                     MSG_COUNTER += 1;
                     msg.add(MSG_COUNTER);
                     msg.add(myPort);
+
                     for (String port : REMOTE_PORTS) {
+
                         Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}), Integer.parseInt(port));
-                        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-                        out.writeObject(msg); //<msg, MSG_COUNTER, MyPort>
 
-                        Log.d("CLIENT",msg.toString() + "\t 1");
-
-                        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-/*
-                        if (in.readUTF().trim().equals("ACK")){
-                            Log.d("ACK","ACK received for " + msgToSend);
-                            in.close();
-                            out.flush();
-                            out.close();
-                            socket.close();
-                        }
-*/
-                        ArrayList<Object> input_list = (ArrayList<Object>) in.readObject();
-                        Log.d("CLIENT",input_list.toString() + "\t 4");
-                        //ArrayList<Object> input_list = new ArrayList<Object>(Arrays.asList(input.split(","))); //<MSG, MSG_ID, P_ID, SEQUENCE>
-                        Integer sequence_proposal = new Integer(String.valueOf(input_list.get(3)));
-                        sequence_list.add(sequence_proposal);
-                        if (sequence_list.size()==5){
-                            final_sequence_number = Collections.max(sequence_list);
-                            Log.d("CLIENT","All replies recieved" + "\t 5");
-                        }
-                        in.close();
+                        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                        out.writeUTF(msg.toString()); //<msg, MSG_COUNTER, MyPort>
                         out.flush();
-                        out.close();
+                        //out.close(); //only the last occurance of close() is executed to keep the socket alive
+                        Log.d("CLIENT",msg.toString() + "\t 1"); //<msg, MSG_COUNTER, MyPort>
+
+                        DataInputStream in = new DataInputStream(socket.getInputStream());
+                        String inp = in.readUTF();
+                        //Thread.sleep(200);
+                        in.close();
                         socket.close();
+
+                        if (null != inp) {
+                            Log.d("CLIENT", inp + "\t 4"); //<MSG, MSG_ID, P_ID, SEQUENCE>
+
+                            ArrayList<Object> input_list = str_to_arrlist(inp);
+                            Integer sequence_proposal = (Integer) input_list.get(3);
+                            sequence_list.add(sequence_proposal);
+
+                            if (sequence_list.size() == 5) {
+                                final_sequence_number = Collections.max(sequence_list);
+                                Log.d("CLIENT", "All replies recieved" + "\t 5");
+                            }
+                        }
+
                     }
                 }
+
                 //multicasting <M,Sequence>
                 ArrayList<Object> msg_seq = new ArrayList<Object>();
                 msg_seq.add(msgToSend);
                 msg_seq.add(MSG_COUNTER);
                 msg_seq.add(myPort);
                 msg_seq.add(final_sequence_number);
+
                 for (String port : REMOTE_PORTS) {
                     Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}), Integer.parseInt(port));
-                    ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-                    out.writeObject(msg_seq); //<msg, MSG_COUNTER, MyPort, Final_sequence number>
-                    Log.d("CLIENT",msg_seq.toString() + "\t 6");
+
+                    DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                    out.writeUTF(msg_seq.toString()); //<msg, MSG_COUNTER, MyPort, Final_sequence number>
                     out.flush();
+
+                    Thread.sleep(500); //Since the socket seems to close before readUTF in server is completed, it throws EOFException. So, the sleep is added to keep the socket alive for some time.
                     out.close();
+                    Log.d("CLIENT",msg_seq.toString() + "\t 6"); //<msg, MSG_COUNTER, MyPort, Final_sequence number>
+
                     socket.close();
                 }
 
@@ -285,24 +324,61 @@ public class GroupMessengerActivity extends Activity{
                 Log.e("CLIENT", "ClientTask UnknownHostException");
             } catch (IOException e) {
                 Log.e("CLIENT", "ClientTask socket IOException");
-            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+
             return null;
         }
-    }
-    class CustomComparator implements Comparator<ArrayList<Object>> {
 
-        // Overriding compare()method of Comparator
-        // for descending order of cgpa
-        public int compare(ArrayList<Object> arr1, ArrayList<Object> arr2) {
-            int i1 = Integer.parseInt(arr1.get(3).toString()), i2 = Integer.parseInt(arr2.get(3).toString());
-            if (i1 > i2)
-                return 1;
-            else if (i1 < i2)
-                return -1;
-            return 0;
+    }
+
+
+    public static ArrayList<Object> str_to_arrlist(String inp){
+
+        inp = inp.substring(1,inp.length()-1);
+        String[] inp_lst = inp.split(",");
+
+        ArrayList<Object> arr = new ArrayList<Object>();
+
+        String msg = inp_lst[0].trim();
+        Integer M_ID = Integer.parseInt(inp_lst[1].trim());
+        String pid = inp_lst[2].trim();
+
+        arr.add(msg);
+        arr.add(M_ID);
+        arr.add(pid);
+
+        if (inp_lst.length==4) {
+            Integer seq = Integer.parseInt(inp_lst[3].trim());
+            arr.add(seq);
         }
-    }
 
+//        Boolean status = Boolean.parseBoolean(inp_lst[4].trim());
+//        arr.add(status);
+
+        return arr;
+    }
 }
+
+class CustomComparator implements Comparator<ArrayList<Object>> {
+
+    // Overriding compare()method of Comparator
+    // for descending order of cgpa
+
+    public int compare(ArrayList<Object> arr1, ArrayList<Object> arr2) {
+
+        int i1 = Integer.parseInt(arr1.get(3).toString()), i2 = Integer.parseInt(arr2.get(3).toString());
+
+        if (i1 > i2)
+            return 1;
+
+        else if (i1 < i2)
+            return -1;
+
+        return 0;
+    }
+}
+
+
